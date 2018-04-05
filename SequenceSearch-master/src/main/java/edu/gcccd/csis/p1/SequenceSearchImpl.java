beddd4f74821663a8f1c0097d92e764343d3c0a3
AdArbitrium <alfred.t.hernandez@gmail.com>
2018-04-04 21:11:14 -0700

    SequenceSearchHomework

--- SequenceSearch-master/src/main/java/edu/gcccd/csis/p1/SequenceSearchImpl.java
+++ SequenceSearch-master/src/main/java/edu/gcccd/csis/p1/SequenceSearchImpl.java
@@ -1,5 +1,7 @@
 package edu.gcccd.csis.p1;
 
+import java.util.ArrayList;
+
 public class SequenceSearchImpl extends SequenceSearch {
 
     public SequenceSearchImpl(final String content, final String start, final String end) {
@@ -8,43 +10,62 @@ public class SequenceSearchImpl extends SequenceSearch {
 
     @Override
     public String[] getAllTaggedSequences() {
-        int startTagPosition = startTag.length();
-        int endTagPosition = 0;
+        ArrayList<Number> listOfStarts = new ArrayList<Number>();
+        ArrayList<Number> listOfEnds= new ArrayList<Number>();
+        String copyOfContent = content;
+        String[] taggedSequences = null;
+        int startIndex = 0;
 
-        String [] tagged = new String [numberOfTaggedSqueuences()];
-        for (int i = 0; i < tagged.length; i++){
-            startTagPosition =+ content.indexOf(startTag);
-            if (startTagPosition == endTagPosition){
-                startTagPosition =+ content.indexOf(startTag);
+        while (startIndex != -1){
+            startIndex = -startTag.length();
+            int endIndex = 0;
+            startIndex = copyOfContent.indexOf(startTag, startIndex + startTag.length());
+            if (startTag == endTag){
+                endIndex = copyOfContent.indexOf(startTag,startIndex + startTag.length());
+            }else{
+                endIndex = copyOfContent.indexOf(endTag, endIndex + endTag.length());
             }
-            if (endTagPosition == startTagPosition){
-                endTagPosition =+ content.indexOf(endTag);
+            if (endIndex < startIndex) {
+                endIndex = copyOfContent.indexOf(endTag, endIndex + endTag.length());
             }
-            endTagPosition =+ content.indexOf(endTag);
+            String temp = "";
+          if (endIndex == -1 ){
+              break;
+          }
+            temp = copyOfContent.substring(startIndex + startTag.length(),endIndex);
+          if (startIndex != endIndex){
+              int firstInstanceOfStart = content.indexOf(startTag,0);
+              int secondInstanceOfStart = content.indexOf(startTag,firstInstanceOfStart+startTag.length());
+              int firstInstanceOfEnd = content.indexOf(endTag,0);
+              int differenceOfStartTags = firstInstanceOfStart - secondInstanceOfStart;
+              int differenceOfStartToEnd = firstInstanceOfStart - firstInstanceOfEnd;
 
-            tagged[i] = content.substring(startTagPosition, endTagPosition);
+              if ( differenceOfStartTags >= differenceOfStartToEnd){
+                  while (startIndex < endIndex && copyOfContent.indexOf(startTag, startIndex + startTag.length()) < endIndex){
+                      startIndex = copyOfContent.indexOf(startTag, startIndex + startTag.length());
+                      temp = copyOfContent.substring(startIndex + startTag.length(),endIndex);
+                  }
+              }
+          }
+
+          taggedSequences = SequenceSearch.adds(taggedSequences, temp);
+          copyOfContent = copyOfContent.substring(endIndex+ endTag.length(), copyOfContent.length());
         }
-      return tagged;
-    }
-
-    public int numberOfTaggedSqueuences(){
-        int counter = 0;
-        int i = -startTag.length();
-        do{
-            if (-1 < (i = content.indexOf(startTag, i + startTag.length()))){
-                counter++;
-            }
-        }while(-1 != i);
-        return counter;
+        if (taggedSequences == null)
+            taggedSequences = new String[0];
+        return taggedSequences;
     }
 
     @Override
     public String getLongestTaggedSequence() {
-        String longest = "";
         String [] temp = getAllTaggedSequences().clone();
-        for (int i = 0; i < temp.length-2 ; i++){
-            if(temp[i].length() > temp[i + 1].length()){
-                temp[i] = longest;
+        String longest = null;
+        if (temp.length > 0) {
+            longest = temp[0];
+            for (int i = 1; i < temp.length; i++) {
+                if (temp[i].length() >= longest.length()) {
+                    longest = temp[i];
+                }
             }
         }
         return longest;
@@ -52,17 +73,28 @@ public class SequenceSearchImpl extends SequenceSearch {
 
     @Override
     public String displayStringArray() {
-       String [] temp = getAllTaggedSequences();
-       String display = "";
-       for (int i = 0; i< temp.length; i++){
-           display += temp[i] + " : " + temp[i].length();
-       }
-        return display;
+        String [] temp = getAllTaggedSequences();
+        String display = "";
+        for (String s:temp){
+            display = display + s + " : " + s.length() + "\n";
+        }
+      return display;
     }
 
     @Override
     public String toString() {
-        return null;
+        int beginIndex = 0;
+        int endIndex = 0;
+        String temp = content;
+        beginIndex = temp.indexOf(startTag, beginIndex);
+        endIndex = temp.indexOf(endTag, endIndex);
+        while (beginIndex != -1 && endIndex !=-1) {
+            temp =  temp.substring(0, beginIndex)
+                    + temp.substring(beginIndex + startTag.length(), endIndex)
+                    + temp.substring(endIndex + endTag.length(), temp.length());
+            beginIndex = temp.indexOf(startTag, beginIndex);
+            endIndex = temp.indexOf(endTag, endIndex);
+        }
+        return temp;
     }
-
 }
